f7a86ac0fff785e6be92938d168f040e70c2093b
vinny@charmswraith <vincentdanso05@gmail.com>
2023-10-22 16:52:14 +0000

    adds map data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from "react";
 
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+];
+
 const App = () => {
   return (
     <div className="app">
@@ -24,8 +29,21 @@ function Form() {
 }
 
 function PackingList() {
-  return <div className="list">LIST</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
 }
+
+function Item({ item }) {
+  return <li>{item.description}</li>;
+}
+
 function Stats() {
   return (
     <footer className="stats">
